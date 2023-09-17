6dc9a954220075564f027d1736ee1f778d0c0a42
Ahmed Waqar <uchihawa@gmail.com>
2023-09-17 17:47:11 +0500

    fix server port issues

--- fullstackreact/src/main.jsx
+++ fullstackreact/src/main.jsx
@@ -4,10 +4,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 //import App from "./App.jsx";
 import "./index.css";
-const promise = axios.get('http://localhost:5174/notes')
+const promise = axios.get('http://localhost:5173/notes')
 
 promise.then(response => {
-  console.log(response)
+  console.log(response.data)
 })
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
