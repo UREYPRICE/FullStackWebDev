99a605306f9ea6d68ed1db7a889bc249d732de80
Ahmed Waqar <37536931+UREYPRICE@users.noreply.github.com>
2023-09-17 12:08:57 +0000

    Add axios

--- fullstackreact/src/main.jsx
+++ fullstackreact/src/main.jsx
@@ -1,12 +1,16 @@
+
+import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+//import App from "./App.jsx";
 import "./index.css";
+const promise = axios.get('http://localhost:5174/notes')
 
-
-
+promise.then(response => {
+  console.log(response)
+})
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App/>
+  
   </React.StrictMode>
 );
